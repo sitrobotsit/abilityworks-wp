27e6b7ab43d775295856c5337cd2e7afb7c8aace
Luke Wilson <lukeuiwilson@gmail.com>
2026-07-09 13:22:45 +0200

    test deploy

--- wp-content/themes/abilityworks21/footer.php
+++ wp-content/themes/abilityworks21/footer.php
@@ -18,7 +18,7 @@
               <?php endforeach; ?>
             </div>
           </div>
-          <div class="logos-carousel-arrows"></div>
+          <div class="logos-carousel-arrows luke-test-deploy"></div>
         </div>
       </div>
     </section>
