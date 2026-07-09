2eb3a81e4e06ec562b3e7e4fe03497a47539ff5c
Luke Wilson <lukeuiwilson@gmail.com>
2026-07-09 16:53:26 +0200

    Update WEBDAV_URL in staging workflow and modify footer class for logos carousel

--- wp-content/themes/abilityworks21/footer.php
+++ wp-content/themes/abilityworks21/footer.php
@@ -18,7 +18,7 @@
               <?php endforeach; ?>
             </div>
           </div>
-          <div class="logos-carousel-arrows sitrobot-test-deploy-99"></div>
+          <div class="logos-carousel-arrows sit-lets-deploy-99"></div>
         </div>
       </div>
     </section>
