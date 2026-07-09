d4e2feb62ac29c0b9589bbfc0054ad92d4075dfc
Luke Wilson <lukeuiwilson@gmail.com>
2026-07-09 14:29:52 +0200

    Update footer class for logos carousel in test deployment

--- wp-content/themes/abilityworks21/footer.php
+++ wp-content/themes/abilityworks21/footer.php
@@ -18,7 +18,7 @@
               <?php endforeach; ?>
             </div>
           </div>
-          <div class="logos-carousel-arrows sitrobot-test-deploy"></div>
+          <div class="logos-carousel-arrows sitrobot-test-deploy-2"></div>
         </div>
       </div>
     </section>
